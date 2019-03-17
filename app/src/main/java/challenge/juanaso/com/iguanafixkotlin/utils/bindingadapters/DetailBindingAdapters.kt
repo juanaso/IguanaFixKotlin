b75271898aa6f157058f9d09788e776ea5f501ab
juanaso <sulbaranja@gmail.com>
2019-03-17 16:31:26 -0300

    binded Phones

--- app/src/main/java/challenge/juanaso/com/iguanafixkotlin/utils/bindingadapters/DetailBindingAdapters.kt
+++ app/src/main/java/challenge/juanaso/com/iguanafixkotlin/utils/bindingadapters/DetailBindingAdapters.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import challenge.juanaso.com.iguanafixkotlin.R
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
+import android.widget.TextView
 import challenge.juanaso.com.iguanafixkotlin.model.User
 import challenge.juanaso.com.iguanafixkotlin.utils.extension.getParentActivity
 import com.bumptech.glide.Glide
@@ -27,6 +28,31 @@ fun setUserPhotoUrl(view: ImageView, user: MutableLiveData<User>?) {
             }else {
                 GlideToVectorYou.justLoadImage(parentActivity, Uri.parse(user.value?.photoUrl), view)
             }
+        })
+    }
+}
+
+@BindingAdapter("userName")
+fun setUserName(view: TextView, user: MutableLiveData<User>?) {
+    val parentActivity:AppCompatActivity? = view.getParentActivity()
+    if(parentActivity != null) {
+        user?.observe(parentActivity, Observer { value -> view.text =  value?.fullName})
+    }
+}
+
+@BindingAdapter("userPhones")
+fun setUserPhones(view: TextView, user: MutableLiveData<User>?) {
+    val parentActivity:AppCompatActivity? = view.getParentActivity()
+    if(parentActivity != null) {
+        user?.observe(parentActivity, Observer { value ->
+            val stringBuilder = StringBuilder()
+            for (phone in user.value?.phones!!) {
+                if (phone.number != null) {
+                    stringBuilder.append(phone.numberOrdered())
+                    stringBuilder.append(System.getProperty("line.separator"))
+                }
+            }
+            view.text = stringBuilder.toString()
         })
     }
 }
